4b2c6a77a2dfbf622e104e257597b0b5936602e6
truongbt <bui.tuan.truong@sun-asterisk.com>
2025-05-09 16:25:44 +0700

    Query with tab

--- webapp/controller/employee/Resume.controller.js
+++ webapp/controller/employee/Resume.controller.js
@@ -1,36 +1,75 @@
-sap.ui.define(["fiori/routing/controller/BaseController"], function (BaseController) {
-    "use strict";
+sap.ui.define(
+    ['fiori/routing/controller/BaseController', 'sap/ui/model/json/JSONModel'],
+    (BaseController, JSONModel) => {
+        'use strict';
 
-    return BaseController.extend("fiori.routing.controller.employee.Resume", {
-        onInit() {
-            let oRouter = this.getRouter();
-            oRouter.getRoute("employeeResume").attachMatched(this._onRouteMatched, this);
-        },
+        let _aValidTabKeys = ['Info', 'Projects', 'Hobbies', 'Notes'];
 
-        _onRouteMatched(oEvent) {
-            let oArgs, oView;
+        return BaseController.extend('fiori.routing.controller.employee.Resume', {
+            onInit() {
+                let oRouter = this.getRouter();
+                this.getView().setModel(new JSONModel(), 'view');
+                oRouter.getRoute('employeeResume').attachMatched(this._onRouteMatched, this);
+            },
 
-            oArgs = oEvent.getParameter("arguments");
-            oView = this.getView();
-            oView.bindElement({
-                path: `/Employees(${oArgs.employeeId})`,
-                events: {
-                    change: this._onBindingChange.bind(this),
-                    dataRequested: (oEvent) => {
-                        oView.setBusy(true);
+            onNavBack() {
+                let oCtx = this.getView().getBindingContext();
+                console.log(oCtx);
+                
+
+                this.getRouter().navTo('employee', {
+                    employeeId: oCtx.getProperty('EmployeeID')
+                });
+            },
+
+            _onRouteMatched(oEvent) {
+                let oArgs, oView, oQuery;
+
+                oArgs = oEvent.getParameter('arguments');
+                oView = this.getView();
+                oView.bindElement({
+                    path: `/Employees(${oArgs.employeeId})`,
+                    events: {
+                        change: this._onBindingChange.bind(this),
+                        dataRequested: (oEvent) => {
+                            oView.setBusy(true);
+                        },
+                        dataReceived: (oEvent) => {
+                            oView.setBusy(false);
+                        },
                     },
-                    dataReceived: (oEvent) => {
-                        oView.setBusy(false);
-                    },
-                },
-            });
-        },
+                });
 
-        _onBindingChange(oEvent) {
-            // No data for the binding
-            if (!this.getView().getBindingContext()) {
-                this.getRouter().getTargets().display("notFound");
-            }
-        },
-    });
-});
+                oQuery = oArgs['?query'];
+                if (oQuery && _aValidTabKeys.indexOf(oQuery.tab) > -1) {
+                    oView.getModel('view').setProperty('/selectedTabKey', oQuery.tab);
+                } else {
+                    // the default query param should be visible at all time
+                    this.getRouter().navTo('employeeResume', {
+                        employeeId: oArgs.employeeId,
+                        '?query': {
+                            tab: _aValidTabKeys[0],
+                        },
+                    });
+                }
+            },
+
+            _onBindingChange(oEvent) {
+                // No data for the binding
+                if (!this.getView().getBindingContext()) {
+                    this.getRouter().getTargets().display('notFound');
+                }
+            },
+
+            onTabSelect(oEvent) {
+                let oCtx = this.getView().getBindingContext();
+                this.getRouter().navTo('employeeResume', {
+                    employeeId: oCtx.getProperty('EmployeeID'),
+                    '?query': {
+                        tab: oEvent.getParameter('selectedKey'),
+                    },
+                });
+            },
+        });
+    }
+);
